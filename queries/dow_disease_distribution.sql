90740130fdf33d7a5c8ca6bd8b18691cfa3dc658
Derek Van Assche <dvanassc@umich.edu>
2013-02-12 16:29:05 -0500

    Initial commit of query files.

---
+++ queries/dow_disease_distribution.sql
@@ -0,0 +1,4 @@
+select strftime('%w', report_date), disease, count(*)
+from health_data_csv
+where date(report_date) between date('2012-08-05') and ('2012-10-27')
+group by strftime('%w', report_date), disease
