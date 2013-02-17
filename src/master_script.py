b1d7721a595b1aeee07f914db3fcaf59f4f08cb1
Derek Van Assche <dvanassc@umich.edu>
2013-02-16 21:56:17 -0500

    Add documentation to master_script.py

--- src/master_script.py
+++ src/master_script.py
@@ -4,6 +4,11 @@ Created on Feb 16, 2013
 @author: Derek
 '''
 
+## 1 - Load the csv/json files into ../HealthDataMap/health.db
 execfile('load_hd_map.py')
+
+## 2 - Create an outbreak table that compresses diseases by date
 execfile('make_outbreak_table.py')
+
+## 3 - Run all sql queries found in the ../queries folder
 execfile('run_simple_queries.py')
