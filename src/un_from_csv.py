0e97081a3a5020d6225b39d782618f8c6feed918
Allison <ambail@umich.edu>
2013-02-18 18:16:42 -0500

    Fix encoding issue

--- src/un_from_csv.py
+++ src/un_from_csv.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys, csv
 import sqlite3 as sqlite
 '''python2 un_from_csv.py
@@ -5,22 +8,27 @@ import sqlite3 as sqlite
 Draft by Allison Bailey (ambail)
 
 Purpose: Create Population Table Based on UN CSV for 2012 Population Estimates
-(Made in 2010), obtained from Guardian UK: 
-http://www.guardian.co.uk/news/datablog/2011/may/06/world-population-country-un#data
-
-Apparently, the UN only comes out with new estimates every
+(Made in 2010). Apparently, the UN only comes out with new estimates every
 two years - we're in between updates.
-'''
 
-with open('../HealthDataMap/un_pop_data.csv','rU') as csvfile:
+References: http://stackoverflow.com/questions/2969044/python-string-escape-vs-unicode-escape
+'''
+def make_unicode(x):
+    try:
+        return unicode(x)
+    except UnicodeDecodeError:
+        byte_text = str(x).encode('string_escape')
+        return unicode(byte_text)
+
+
+with open('../HealthMapData/un_pop_data.csv','rU') as csvfile:
     popreader = csv.reader(csvfile)
     data = []
     for row in popreader:
-        l =(row[0],row[1],row[65])
+        l =(make_unicode(row[0]),make_unicode(row[1]),make_unicode(row[65]))
         data.append(l)        
         
-with sqlite.connect('../HealthDataMap/health.db') as con: 
-  con.text_factory = str
+with sqlite.connect('../HealthMapData/health.db') as con: 
   cur = con.cursor() 
   cur.execute("DROP TABLE IF EXISTS populations")
   cur.execute("CREATE TABLE populations(Country TEXT, ISO_Code TEXT, Population_2012 INTEGER)")
