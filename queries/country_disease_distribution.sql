2dea03997f7da3f6e90701065c67939de02a2b88
Anna <ampeter@umich.edu>
2013-02-19 19:46:20 -0500

    Updated to pull from outbreaks table instead of main table

--- queries/country_disease_distribution.sql
+++ queries/country_disease_distribution.sql
@@ -1,4 +1,4 @@
 -- Pull population data from http://en.wikipedia.org/wiki/List_of_countries_by_population to get a per capita?
 select country, count(disease) as DISEASE_COUNT
-from health_data_csv
+from outbreaks
 group by country
