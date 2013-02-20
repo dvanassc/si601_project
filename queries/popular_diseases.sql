3d167dff5566af325e7aa396920efbbbee1daf21
Anna <ampeter@umich.edu>
2013-02-20 11:47:12 -0500

    Added field - total reports per disease

--- queries/popular_diseases.sql
+++ queries/popular_diseases.sql
@@ -1,4 +1,4 @@
-select disease, count(*) as OUTBREAK_COUNT
+select disease, count(*) as OUTBREAK_COUNT, sum(report_instances) as TOTAL_REPORTS
 from outbreaks
 group by disease
 order by count(*) desc
