23ce9e4173770cd747e21e6c57cd97dfeab4ac8e
ambailson <ambailson@gmail.com>
2013-02-20 09:27:11 -0500

    Delete comment

--- queries/outbreaks_percap.sql
+++ queries/outbreaks_percap.sql
@@ -1,8 +1,4 @@
-/*ALLISON -- Currently this query yields unexpected results for the percap
-calculation -- I'll continue to investigate, but let me know if you see any
-obvious errors.*/
-
-select o.country, count(o.disease) as incidents,p.population_2012,(count(o.disease)/p.population_2012) as incident_percap
+select o.country, count(*) as incidents,p.population_2012,(count(*)/p.population_2012) as incident_percap
 from outbreaks as o
 join populations as p on (o.country=p.country)
 group by o.country
