c4be42962d3f6033e182cda31d32c40b1996638f
Anna <ampeter@umich.edu>
2013-02-20 11:24:17 -0500

    Changed field name to OUTBREAK_COUNT

--- queries/country_disease_distribution.sql
+++ queries/country_disease_distribution.sql
@@ -1,4 +1,4 @@
 -- Pull population data from http://en.wikipedia.org/wiki/List_of_countries_by_population to get a per capita?
-select country, count(disease) as DISEASE_COUNT
+select country, count(disease) as OUTBREAK_COUNT
 from outbreaks
 group by country
